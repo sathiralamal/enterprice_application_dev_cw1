38102d81a8d00616d950d3d474fab8261ad6113b
sathiralamal <sdsat756@gmail.com>
2023-07-03 16:52:56 +0530

    add graphs

--- FitnessTrackingApplication/Views/SubViews/DashbordView.Designer.cs
+++ FitnessTrackingApplication/Views/SubViews/DashbordView.Designer.cs
@@ -28,19 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            cartesianChart1 = new LiveChartsCore.SkiaSharpView.WinForms.CartesianChart();
+            label1 = new Label();
+            label2 = new Label();
+            pieChart1 = new LiveChartsCore.SkiaSharpView.WinForms.PieChart();
             SuspendLayout();
+            // 
+            // cartesianChart1
+            // 
+            cartesianChart1.Location = new Point(55, 95);
+            cartesianChart1.Name = "cartesianChart1";
+            cartesianChart1.Size = new Size(384, 196);
+            cartesianChart1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(55, 40);
+            label1.Name = "label1";
+            label1.Size = new Size(325, 25);
+            label1.TabIndex = 2;
+            label1.Text = "Cheat Meal vs Workout (Last month)";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 13F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(494, 40);
+            label2.Name = "label2";
+            label2.Size = new Size(150, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Workout Details";
+            // 
+            // pieChart1
+            // 
+            pieChart1.InitialRotation = 0D;
+            pieChart1.IsClockwise = true;
+            pieChart1.Location = new Point(494, 81);
+            pieChart1.MaxAngle = 360D;
+            pieChart1.Name = "pieChart1";
+            pieChart1.Size = new Size(217, 196);
+            pieChart1.TabIndex = 4;
+            pieChart1.Total = null;
+            pieChart1.Load += pieChart1_Load;
             // 
             // DashbordView
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(927, 494);
+            Controls.Add(pieChart1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(cartesianChart1);
             Name = "DashbordView";
             Text = "Dashbord";
             Load += DashbordView_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private LiveChartsCore.SkiaSharpView.WinForms.CartesianChart cartesianChart1;
+        private Label label1;
+        private Label label2;
+        private LiveChartsCore.SkiaSharpView.WinForms.PieChart pieChart1;
     }
 }
--- FitnessTrackingApplication/Views/SubViews/DashbordView.cs
+++ FitnessTrackingApplication/Views/SubViews/DashbordView.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using LiveCharts.WinForms;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,21 +10,137 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using LiveChartsCore.SkiaSharpView.Painting;
+using SkiaSharp;
+using FitnessTrackingApplication.Controller;
+using FitnessTrackingApplication.Models;
 
 namespace FitnessTrackingApplication.Views
+
 {
     public partial class DashbordView : Form
     {
+        WorkoutController workoutController;
+        MealController mealController;
+        UserController userControl;
+        User user;
+
+        List<Meal> LastWeekMeal;
+        List<Meal> LastMonthMeal;
+
+        List<Workout> LastWeekWorkout;
+        List<Workout> LastMonthkWorkout;
+
+        double TotalBurnCalories = 0;
+        double TotalEatCalories = 0;
+
         public DashbordView()
         {
             InitializeComponent();
             this.FormBorderStyle = FormBorderStyle.None;
+
+            workoutController = new WorkoutController();
+            mealController = new MealController();
+            userControl = new UserController();
+
+            LastMonthMeal = mealController.GetLastMonthMealWithFood();
+            LastWeekMeal = mealController.GetLastWeekMealWithFood();
+
+            LastWeekWorkout = workoutController.GetAllLastWeek();
+            LastMonthkWorkout = workoutController.GetAllLastMonth();
+
+            
+
+
         }
+
+        private double[] getMealTotalCaloryList(List<Meal> meals)
+        {
+            List<double> caloryList = new List<double>();
+            meals.ForEach(m =>
+            {
+                caloryList.Add(m.TotalCatories);
+            });
+            return caloryList.ToArray();
+        }
+
+        private double[] getWorkoutTotalCaloryList(List<Workout> meals)
+        {
+            List<double> caloryList = new List<double>();
+            meals.ForEach(m =>
+            {
+                caloryList.Add(m.TotalCalories);
+            });
+            return caloryList.ToArray();
+        }
+
 
         private void DashbordView_Load(object sender, EventArgs e)
         {
-           
+            double[] dataMeal1 = getMealTotalCaloryList(LastMonthMeal);
+            double[] dataWorkouts = getWorkoutTotalCaloryList(LastMonthkWorkout);
+
+            ShowLineChart1(dataMeal1, dataWorkouts);
+            ShowPiChart1(LastMonthMeal, LastMonthkWorkout);
+        }
+
+
+
+        private void ShowLineChart1(double[] data1, double[] data2)
+        {
+            cartesianChart1.Series = new ISeries[]
+            {
+                new LineSeries<double>
+                {
+                    Values = data1,
+                    Stroke = new SolidColorPaint(SKColors.Blue) { StrokeThickness = 2 },
+                    Fill = null,
+                    Name = "Meal Total calories",
+
+                },
+                 new LineSeries<double>
+                {
+                    Values = data2,
+                    Stroke = new SolidColorPaint(SKColors.IndianRed) { StrokeThickness = 2 },
+                    Fill = null,
+                    Name = "Workout Burn calories",
+
+
+                }
+            };
+        }
+
+
+        private void ShowPiChart1(List<Meal> meatData, List<Workout> workoutsData)
+        {
+            meatData.ForEach(m =>
+            {
+                TotalEatCalories = TotalEatCalories + m.TotalCatories;
+            });
+
+            workoutsData.ForEach(m =>
+            {
+                TotalBurnCalories = TotalBurnCalories + m.TotalCalories;
+            });
+
+
+            pieChart1.Series = new List<ISeries>
+             {
+                 new PieSeries<double>
+                  {
+                       Values = new List<double> { 2 },
+                       Pushout = 8,
+                       Stroke = new SolidColorPaint(SKColors.Red)
+                    },
+                new PieSeries<double> { Values = new List<double> { TotalBurnCalories } },
+                new PieSeries<double> { Values = new List<double> { TotalEatCalories } }
+
+               };
+        }
+
+        private void pieChart1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
